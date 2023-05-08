c3c20c4bb33e6dddb5708cda38588df32112054c
laksamanaap <laksamana.arya1412@gmail.com>
2023-05-08 16:29:20 +0700

    Register token API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,20 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class); 
+// Route::resource('products', ProductController::class); 
 // => To get all routes
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/products', [ProductController::class, 'index']);
 
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-// Route::get('/products', [ProductController::class, 'index']);
-
-// Route::post('/products', [ProductController::class, 'store']);
-
-// Route::get('/products/{id}', [ProductController::class, 'show']);
-
-// Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/produtcs/{id}', [ProductController::class, 'destroy']);
 });
