690b3f26b1578176a10ab0ff2de06d7814c97f14
laksamanaap <laksamana.arya1412@gmail.com>
2023-05-11 13:54:53 +0700

    Add Sanctum for local api's

--- routes/api.php
+++ routes/api.php
@@ -28,26 +28,31 @@ Route::get('/greet2', [UserController::class, 'greet2']);
 // Non auth API's (OA Swagger)
 Route::get('/products', [UserController::class, 'index']);
 
+Route::get('/products/{id}', [UserController::class, 'show']);
+
 Route::get('/products/search/{name}', [UserController::class, 'search']);
 
-Route::get('/products/{id}', [UserController::class, 'show']);
+Route::post('/products', [UserController::class, 'store']);  
+
+Route::delete('/products/{id}', [UserController::class, 'destroy']);
+
+Route::put('/products', [UserController::class, 'update']);
+
+
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
-
-Route::post('/logout', [AuthController::class, 'logout']);
-Route::delete('/products/{id}', [UserController::class, 'destroy']);
-Route::put('/products', [UserController::class, 'update']);
-Route::post('/products', [UserController::class, 'store']);  
 // Non auth API's (OA Swagger)
 
 
-// API's w Auth
-Route::group(['middleware' => ['auth:sanctum']], function() { 
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-    Route::put('/products', [ProductController::class, 'update']);
-    Route::post('/products', [ProductController::class, 'store']);
-}); 
-// API's w Auth
+// API's w sanctum (for local API's)
+// Route::group(['middleware' => ['auth:sanctum']], function() { 
+//     Route::post('/logout', [AuthController::class, 'logout']);
+//     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+//     Route::put('/products', [ProductController::class, 'update']);
+//     Route::post('/products', [ProductController::class, 'store']);
+// }); 
+// API's w sanctum (for local API's)
+
